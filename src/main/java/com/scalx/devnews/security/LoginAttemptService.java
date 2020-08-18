2da813d5be9ea1c721fc180f6f7f7b7618ab2d26
kutaybuyukkorukcu <kutaybuyukkorukcu@gmail.com>
2020-08-18 15:20:18 +0300

    logged users ready to implement as cache list

---
+++ src/main/java/com/scalx/devnews/security/LoginAttemptService.java
@@ -0,0 +1,53 @@
+package com.scalx.devnews.security;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class LoginAttemptService {
+
+    private final int MAX_ATTEMPT = 10;
+    private LoadingCache<String, Integer> cacheList;
+
+    public LoginAttemptService() {
+        super();
+
+        cacheList = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.DAYS)
+                .build(new CacheLoader<String, Integer>() {
+                    @Override
+                    public Integer load(String s) throws Exception {
+                        return 0;
+                    }
+                });
+    }
+
+    public void loginSucceeded(String key) {
+        cacheList.invalidate(key);
+    }
+
+    public void loginFailed(String key) {
+        int attempts = 0;
+
+        try {
+            attempts = cacheList.get(key);
+        } catch (ExecutionException e) {
+            attempts = 0;
+        }
+
+        attempts++;
+        cacheList.put(key, attempts);
+    }
+
+    public boolean isBlocked(String key) {
+        try {
+            return cacheList.get(key) >= MAX_ATTEMPT;
+        } catch (ExecutionException e) {
+            return false;
+        }
+    }
+}
